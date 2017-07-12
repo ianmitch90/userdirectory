786be41eadb15acc549532b32bff51b3b03620fe
ianmitch90 <ianmitch90@gmail.com>
2017-07-12 05:21:15 -0500

    basic node express demo

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-const express = require('express')
-const app = express()
-const mustache = require('mustache-express')
-const userdata = require('../models/user.js')
-const router = require('../routes')
-const data = require('data.js')
 
-app.engine('mustache', mustache())
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+const data = require('./user.js')
+const app = express()
+const bodyparser = require('body-parser')
+app.use(express.static(__dirname + '/public'))
+
+app.engine('mustache', mustacheExpress())
+app.set('views', 'views/')
 app.set('view engine', 'mustache')
 
-app.listen(3000, function(){
-  console.log('sona alert')
+app.get('/', function (req, res) {
+  res.render('index', {users: data.users})
+})
+
+app.get('/:id', function (req, res) {
+  var id = req.params.id - 1;
+  res.render('card', {users: data.users[id]})
+})
+
+app.listen(3000, function () {
+  console.log('sona')
 })
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-const router = express.Router();
-const data = require('/data.js')
-
-app.get('/', function (req, res) {
-  res.send('index')
-})
